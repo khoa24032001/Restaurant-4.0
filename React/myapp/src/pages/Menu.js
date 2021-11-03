590bbca9fe26bf321916ca85ec1bf901885ec9d7
dang-416 <59189549+dang-416@users.noreply.github.com>
2021-11-04 02:30:06 +0700

    Css đêm

--- React/myapp/src/pages/Menu.js
+++ React/myapp/src/pages/Menu.js
@@ -13,54 +13,50 @@ class PickFood extends Component {
         this.adjustItem = this.adjustItem.bind(this);
         //     this.addDrug = this.addDrug.bind(this);
     }
-    notItem(food,item) { 
-        return item.food.food_name==food.food_name; 
-      } 
+    notItem(food, item) {
+        return item.food.food_name == food.food_name;
+    }
     addItem(item) {
         this.setState({
-            cart: this.state.cart.push({food: item, num:1}),
-            totalCost: this.total()
+            cart: this.state.cart.push({ food: item, num: 1 }),
+            totalCost: this.total(),
         });
     }
-    rmvItem(food){
-        const newCart=this.state.cart.filter(
-            (item)=>item!=food)
+    rmvItem(food) {
+        const newCart = this.state.cart.filter((item) => item != food);
         this.setState({
             cart: newCart,
-            totalCost: this.total()
-        }
-        );
+            totalCost: this.total(),
+        });
     }
     adjustItem(foodfix, more) {
-        const A=foodfix;
-        if (more)
-            A.num++;
+        const A = foodfix;
+        if (more) A.num++;
         else A.num--;
-        const newcart=this.state.cart.map(
-            (food)=>{
-                if (food==foodfix) return A;
-                else return food;
-            }
-             )
+        const newcart = this.state.cart.map((food) => {
+            if (food == foodfix) return A;
+            else return food;
+        });
         this.setState({
             cart: newcart,
-            totalCost:this.total()
+            totalCost: this.total(),
         });
     }
 
     addFood(food) {
-        const exist=this.state.cart.filter((item)=>item.food.food_name==food.food_name)
-        if (exist.length>0){
-            this.adjustItem(exist[0],true);
-        }
-        else{
+        const exist = this.state.cart.filter(
+            (item) => item.food.food_name == food.food_name
+        );
+        if (exist.length > 0) {
+            this.adjustItem(exist[0], true);
+        } else {
             this.setState({
-             carts: this.state.cart.push({food:food,num:1}),
-            totalCost:this.total()
-         })
+                carts: this.state.cart.push({ food: food, num: 1 }),
+                totalCost: this.total(),
+            });
         }
-         
-        console.log(this.state)
+
+        console.log(this.state);
     }
 
     handleSubmit() {
@@ -69,54 +65,111 @@ class PickFood extends Component {
         });
         this.setState({ food_display: search });
     }
-    search(type){
-        this.setState(
-            {food_display:this.state.food_list.filter(food=>food.type==type)}
-        )
-    }
-    showAll(){
+    search(type) {
         this.setState({
-            food_display:this.state.food_list
-        })
+            food_display: this.state.food_list.filter(
+                (food) => food.type == type
+            ),
+        });
     }
-    total(){return this.state.cart.reduce((total,item)=>total+item.food.price*item.num,0);}
-                
-     componentDidMount() {
-                this.setState(FoodOrdData);
-                this.setState({food_display:this.state.food_list,
-                    totalCost:this.total()});
-                console.log(this.state)
-       };
+    showAll() {
+        this.setState({
+            food_display: this.state.food_list,
+        });
+    }
+    total() {
+        return this.state.cart.reduce(
+            (total, item) => total + item.food.price * item.num,
+            0
+        );
+    }
+
+    componentDidMount() {
+        this.setState(FoodOrdData);
+        this.setState({
+            food_display: this.state.food_list,
+            totalCost: this.total(),
+        });
+        console.log(this.state);
+    }
 
     render() {
         const food_list = this.state.food_display.map((food) => {
             return (
                 <div className="containCard">
-
-                <Card className="foodCard">
-                        <h5> {food.food_name}</h5>
-                        <h6> Giá tiền : {food.price}</h6>
-                        <Button className="infobtn">Xem chi tiết </Button>
-                            <Button className="addbtn" onClick={(e)=>{this.addFood(food)}}>Thêm vào giỏ hàng</Button>
+                    <Card className="foodCard">
+                        <Row>
+                            <Col>
+                            <div className='menuFoodImg'>
+                                
+                            </div>
+                            </Col>
+                            <Col><div className='menuFoodName'> {food.food_name}</div>
+                        <div className='menuFoodPrice'> Giá tiền : {food.price}</div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Button className="infobtn">Xem chi tiết </Button>
+                        <Button
+                            className="addbtn"
+                            onClick={(e) => {
+                                this.addFood(food);
+                            }}
+                        >
+                            Thêm vào giỏ hàng
+                        </Button>
+                        </Row>
                         
-                </Card></div>
+                        
+                    </Card>
+                </div>
             );
         });
         const cart_food_list = this.state.cart.map((foodItem) => {
             //const price = food.price * 1000;
             return (
                 <Container className="padding10">
-                    <Card className='cartItemContent'>
-                         {/* <img width="91.98px" height="90px" src={FaSearch} alt = "Xem hồ sơ bệnh án"/> */}
-                        
-                        
-                        <div className='img'></div> 
-                        <Row className="incartItem">  {foodItem.food.food_name}
-                        {/* TODO action button to update cart */}
-                        <div className='qty'> <Button onClick={(e)=>{this.adjustItem(foodItem,false)}}>-</Button> {foodItem.num} <Button onClick={(e)=>{this.adjustItem(foodItem,true)}}>+</Button>  </div></Row>
+                    <Card className="cartItemContent">
+                        {/* <img width="91.98px" height="90px" src={FaSearch} alt = "Xem hồ sơ bệnh án"/> */}
+
+                        <div className="img"></div>
+                        <Row className="incartItem">
+                            {" "}
+                            {foodItem.food.food_name}
+                            {/* TODO action button to update cart */}
+                            <div className="qty">
+                                {" "}
+                                <Button className='subCartItem'
+                                    onClick={(e) => {
+                                        this.adjustItem(foodItem, false);
+                                    }}
+                                >
+                                    -
+                                </Button>{" "}
+                                {foodItem.num}{" "}
+                                <Button className='addCartItem'
+                                    onClick={(e) => {
+                                        this.adjustItem(foodItem, true);
+                                    }}
+                                >
+                                    +
+                                </Button>{" "}
+                            </div>
+                        </Row>
                         <Row>
-                        <Input className="note" name="note" type="text"></Input>
-                        <Button className='del' onClick={(e)=>{this.rmvItem(foodItem)}}>Del</Button>
+                            <Input
+                                className="note"
+                                name="note"
+                                type="text"
+                            ></Input>
+                            <Button
+                                className="del"
+                                onClick={(e) => {
+                                    this.rmvItem(foodItem);
+                                }}
+                            >
+                                Del
+                            </Button>
                         </Row>
                     </Card>
                 </Container>
@@ -127,10 +180,41 @@ class PickFood extends Component {
             <Row className="screen">
                 <Col className="MenuCard">
                     <Row>
-                        <Button className="type-button" onClick={(e)=>{this.showAll()}}> Tất cả</Button>
-                        <Button className="type-button"onClick={(e)=>{this.search("DoAn")}}>Món ăn</Button>
-                        <Button className="type-button"onClick={(e)=>{this.search("Nuoc")}}> Nước uống</Button>
-                        <Button className="type-button"onClick={(e)=>{this.search("Combo")}}> Combo</Button>
+                        <Button
+                            className="type-button"
+                            onClick={(e) => {
+                                this.showAll();
+                            }}
+                        >
+                            {" "}
+                            Tất cả
+                        </Button>
+                        <Button
+                            className="type-button"
+                            onClick={(e) => {
+                                this.search("DoAn");
+                            }}
+                        >
+                            Món ăn
+                        </Button>
+                        <Button
+                            className="type-button"
+                            onClick={(e) => {
+                                this.search("Nuoc");
+                            }}
+                        >
+                            {" "}
+                            Nước uống
+                        </Button>
+                        <Button
+                            className="type-button"
+                            onClick={(e) => {
+                                this.search("Combo");
+                            }}
+                        >
+                            {" "}
+                            Combo
+                        </Button>
                     </Row>
 
                     <Row>{food_list}</Row>
@@ -139,10 +223,16 @@ class PickFood extends Component {
                     <HeaderCart />
                     {cart_food_list}
                     <Container>
-                Tổng: <h1>{this.state.totalCost}</h1>
-                {/* <Button txt="Thanh toán" path="pay" /> */}
-            <Button onClick={(e)=>{console.log("đã thanh toán")}}>Thanh toán </Button>
-            </Container>
+                        Tổng: <h1>{this.state.totalCost}</h1>
+                        {/* <Button txt="Thanh toán" path="pay" /> */}
+                        <Button
+                            onClick={(e) => {
+                                console.log("đã thanh toán");
+                            }}
+                        >
+                            Thanh toán{" "}
+                        </Button>
+                    </Container>
                 </Col>
             </Row>
         );
@@ -162,11 +252,9 @@ class CustomerInfo extends Component {
 
         return (
             <h6>
-                <Row> Người dùng: {customerName}
-               </Row>
-               <Row> Vai trò: {Role}</Row>
+                <Row> Người dùng: {customerName}</Row>
+                <Row> Vai trò: {Role}</Row>
             </h6>
         );
     }
 }
-
